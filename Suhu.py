780550dcb5d5dab7dd905044eb06001dd8714be4
Moh. Fariq Firdaus Karim <132199895+polytron1@users.noreply.github.com>
2023-12-19 21:08:34 +0700

    Update Suhu.py

--- Suhu.py
+++ Suhu.py
@@ -10,9 +10,13 @@ c1, c2 = st.columns(2)
  
 with c1:
    x = st.number_input('Suhu = ',value=100)
+   st.write('Dikonversi ke: ')
 with c2:
    satuan =  st.selectbox(
      'satuan',
-     ('C', 'F', 'R', 'K'))
+     ('C', 'F', 'R', 'K'), key='k1')
+   konversi =  st.selectbox(
+     'konversi',
+     ('C', 'F', 'R', 'K'), key='k2')
 
 st.caption('Copyright © Nugroho Adi Pramono 2023')
