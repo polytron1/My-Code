08bebc8facd246a9bf7a5d249f83fd2b3edb663d
Moh. Fariq Firdaus Karim <132199895+polytron1@users.noreply.github.com>
2023-12-19 20:49:20 +0700

    Update komputasi_awan.py

--- komputasi_awan.py
+++ komputasi_awan.py
@@ -9,7 +9,7 @@ st.subheader('Plot')
 nama = st.text_input('Nama', 'Fariq', label_visibility='collapsed')
 st.write('Halo', nama)
 
-f1 = st.number_input('Nilai f1 = ',value=2)
+f1 = st.number_input('Nilai f1 = ',value=1)
 f2 = st.number_input('Nilai f2 = ',value=1)
 st.write('Frekuensi plot sinus adalah ', f1)
 st.write('Frekuensi plot cosinus adalah ', f2)
