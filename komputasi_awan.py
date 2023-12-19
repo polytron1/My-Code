bf09d0c7a243e846f0d13b592d257dbb3071f343
Moh. Fariq Firdaus Karim <132199895+polytron1@users.noreply.github.com>
2023-12-19 20:38:28 +0700

    Update komputasi_awan.py

--- komputasi_awan.py
+++ komputasi_awan.py
@@ -9,14 +9,14 @@ st.subheader('Plot')
 nama = st.text_input('Nama', 'Fariq', label_visibility='collapsed')
 st.write('Halo', nama)
 
-f1 = st.number_input('f1 = ', value=1)
-f2 = st.number_input('f2 = ', value=1)
+f1 = st.number_input('Nilai f1 = ', value=1)
+f2 = st.number_input('Nilai f2 = ', value=1)
 st.write('rekuensi plot sinus adalah ', f1)
 st.write('rekuensi plot cosinus adalah ', f2)
  
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-y = np.sin(x)  # Calculating sin(x) values
-z = np.cos(x)  # Calculating sin(x) values
+y = np.sin(f1 * x)  # Calculating sin(x) values
+z = np.cos(f2 * x)  # Calculating sin(x) values
  
 fig, ax = plt.subplots(figsize=(16, 8))
 ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
@@ -37,7 +37,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.caption('Sin')
     x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.sin(x)  # Calculating sin(x) values
+    y = np.sin(f1*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
     ax.plot(x, y, label='sin(x)', color='b')  # Plotting sin(x) curve
@@ -52,7 +52,7 @@ with col1:
 with col2:
     st.caption('Cos')
     x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
-    y = np.cos(x)  # Calculating sin(x) values
+    y = np.cos(f2*x)  # Calculating sin(x) values
  
     fig, ax = plt.subplots(figsize=(16, 8))
     ax.plot(x, y, label='cos(x)', color='g')  # Plotting cos(x) curve
