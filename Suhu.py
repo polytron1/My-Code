b159f904912d5b21a9d19ff5c24b9228544e6e3a
Moh. Fariq Firdaus Karim <132199895+polytron1@users.noreply.github.com>
2023-12-19 21:05:21 +0700

    Update Suhu.py

--- Suhu.py
+++ Suhu.py
@@ -9,10 +9,10 @@ st.subheader('Plot')
 c1, c2 = st.columns(2)
  
 with c1:
- x = st.number_input('Suhu = ',value=100)
+   x = st.number_input('Suhu = ',value=100)
 with c2:
- satuan =  st.selectbox(
- 'satuan',
- ('C', 'F', 'R', 'K'))
+   satuan =  st.selectbox(
+     'satuan',
+     ('C', 'F', 'R', 'K'))
 
 st.caption('Copyright © Nugroho Adi Pramono 2023')
